ef7a3c162fc8f055e37907caae2ab233f7db19be
Sai num town <sainumtown@gmail.com>
2016-06-29 19:45:39 +0630

    Update Profile user design layout

--- app/src/main/java/xz/sainumtown/week_4_execrise/fragments/LinkedInFragment.java
+++ app/src/main/java/xz/sainumtown/week_4_execrise/fragments/LinkedInFragment.java
@@ -35,7 +35,7 @@ public class LinkedInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_linked_in, container, false);
+        return inflater.inflate(R.layout.material_design_profile_screen_xml_ui_design, container, false);
     }
 
 
